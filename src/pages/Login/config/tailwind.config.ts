a744459317d5c915c51153e9960d8995f09813a2
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2026-03-02 20:42:58 -0300

    feat: created Login tailwind config

---
+++ src/pages/Login/config/tailwind.config.ts
@@ -0,0 +1,20 @@
+export const tailwindConfig = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D4EAFC",
+        "accent-green": "#A3E4A1",
+        "background-light": "#F8F9F3",
+        "background-dark": "#121412",
+      },
+      fontFamily: {
+        display: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "24px",
+        "large": "40px",
+      },
+    },
+  },
+};
