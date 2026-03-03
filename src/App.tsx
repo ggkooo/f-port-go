7ebe36a42c7e652d5d6b289a9d92cfd5af2b2f7f
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2026-03-02 21:30:40 -0300

    fix: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from "./pages/Login"
+import Register from './pages/Register'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   )
