0b55843f9fb9a41a7d0b3b88028e76d54c7affc7
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2026-03-02 21:44:20 -0300

    fix: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from "./pages/Login"
 import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   )
